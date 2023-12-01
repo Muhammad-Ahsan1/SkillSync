666863618e55a0e36f4f05df06eefe13ae3a2c14
Ali Raza <aliraza.12748g@gmail.com>
2023-12-02 00:56:21 +0500

    ready to deploy

--- src/Components/utils/contants.js
+++ src/Components/utils/contants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5500";
+export const BASE_URL = "https://janmbu-space-client.onrender.com";
 // export const BASE_URL = "https://jambu-space-server.herokuapp.com";
 export const API_URL = BASE_URL + "/api/client/";
 

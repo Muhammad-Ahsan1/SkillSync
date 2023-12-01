ad45e7166ed08d15fde61ffd6dc329cfa8d13565
Ali Raza <aliraza.12748g@gmail.com>
2023-12-02 02:21:41 +0500

    add baseurls

--- src/Components/utils/contants.js
+++ src/Components/utils/contants.js
@@ -2,7 +2,7 @@ export const BASE_URL = "https://jambu-space-backend.vercel.app";
 // export const BASE_URL = "https://jambu-space-server.herokuapp.com";
 export const API_URL = BASE_URL + "/api/client/";
 
-export const SOCKET_URL = "ws://jambu-space-socket-server.herokuapp.com";
+export const SOCKET_URL = BASE_URL + "/api/client/";
 
 export const STATUS = {
   PLACED: "PLACED",

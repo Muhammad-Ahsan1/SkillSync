09b1b9d15367275ec7c8dba1f96e4b2c432b45e5
zaeemthebotslayer <zaeemawaistkturners@gmail.com>
2023-12-14 00:01:03 +0500

    increased height

--- src/Components/Pages/admin/AdminDashboard.js
+++ src/Components/Pages/admin/AdminDashboard.js
@@ -137,7 +137,7 @@ export default function AdminDashboard() {
 
   return (
     <>
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: 1000, width: '100%', }}>
       <DataGrid
         rows={data}
         columns={columns}
